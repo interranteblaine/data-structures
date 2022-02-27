7d87e94693cb31dd6c1ef022b0be0edc9dbbbd71
Blaine Interrante <interrante.blaine@gmail.com>
2022-02-27 12:15:32 -0500

    adds implementations for queues and stacks

---
+++ queues/queue-singly-linked-list.js
@@ -0,0 +1,41 @@
+// Implementing a queue using a singly linked list (SLL)
+// requires adding (enqueue) to the end of the SLL
+// and removing (dequeue) from the begining of the SLL
+// so that adding and removing are both O(1)
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+    } else {
+      this.last.next = newNode;
+    }
+    this.last = newNode;
+    return ++this.length;
+  }
+
+  dequeue() {
+    if (!this.first) return undefined;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return temp;
+  }
+}
---
+++ stacks/stack-singly-linked-list.js
@@ -0,0 +1,42 @@
+// Implementing a stack using a singly linked list (SLL)
+// requires adding and removing from the begining of
+// the SLL so that adding and removing are both O(1)
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.length;
+  }
+
+  pop() {
+    if (!this.first) return undefined;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return temp;
+  }
+}
