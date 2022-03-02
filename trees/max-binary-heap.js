a776faa5d338758f133d1abb6f43fddc604206da
Blaine Interrante <interrante.blaine@gmail.com>
2022-03-02 10:36:19 -0500

    implements a max binary heap DS

---
+++ trees/max-binary-heap.js
@@ -0,0 +1,70 @@
+// All child nodes are smaller than their parent nodes.
+// Left children are filled out first.
+// For a parent at index i:
+//      their left child is at index 2i+1
+//      their right child is at index 2i+2
+// For a child at index i:
+//      their parent is at index Math.floor((n-1)/2)
+// Insertion and removal are O(logn), Searching is O(n)
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  insert(elem) {
+    this.values.push(elem);
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.trickleDown();
+    }
+    return max;
+  }
+
+  trickleDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild;
+      let rightChild;
+      let swap = null;
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
