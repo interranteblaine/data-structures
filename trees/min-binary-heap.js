3f3bfc379d481f9aa19495499946fabfd78b05f7
Blaine Interrante <interrante.blaine@gmail.com>
2022-03-02 11:03:51 -0500

    adds implementation of a min binary heap

---
+++ trees/min-binary-heap.js
@@ -0,0 +1,72 @@
+// All child nodes are larger than their parent nodes.
+// Left children are filled out first.
+// For a parent at index i:
+//      their left child is at index 2i+1
+//      their right child is at index 2i+2
+// For a child at index i:
+//      their parent is at index Math.floor((n-1)/2)
+// Insertion and removal are O(logn), Searching is O(n)
+
+class MinBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  insert(elem) {
+    this.values.push(elem);
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element >= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  extractMin() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.trickleDown();
+    }
+    return min;
+  }
+
+  trickleDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild;
+      let rightChild;
+      let swap = null;
+      while (true) {
+        if (leftChildIdx < length) {
+          leftChild = this.values[leftChildIdx];
+          if (leftChild < element) {
+            swap = leftChildIdx;
+          }
+        }
+        if (rightChildIdx < length) {
+          rightChild = this.values[rightChildIdx];
+          if (
+            (swap === null && rightChild < element) ||
+            (swap !== null && rightChild < leftChild)
+          ) {
+            swap = rightChildIdx;
+          }
+        }
+        if (swap === null) break;
+        this.values[idx] = this.values[swap];
+        this.values[swap] = element;
+        idx = swap;
+      }
+    }
+  }
+}
