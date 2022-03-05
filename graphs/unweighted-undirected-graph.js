ba2d834ff31846327e36d782ec142439f3f51420
Blaine Interrante <interrante.blaine@gmail.com>
2022-03-05 18:12:56 -0500

    implements an unweighted undirected graph with tests

---
+++ graphs/unweighted-undirected-graph.js
@@ -0,0 +1,35 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => {
+            return v !== vertex2;
+        });
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => {
+            return v !== vertex1;
+        });
+    }
+
+    removeVertex(vertex) {
+        while(this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
+}
+
+module.exports = Graph;
---
+++ graphs/unweighted-undirected-graph.test.js
@@ -0,0 +1,73 @@
+const Graph = require('./unweighted-undirected-graph');
+
+describe('unweighted undirected graph >', () => {
+    let g;
+    beforeEach(() => {
+        g = new Graph();
+    });
+
+    test('graph uses an adjacency list', () => {
+        expect(g.hasOwnProperty('adjacencyList')).toBe(true);
+    });
+
+    test('addVertex adds a ', () => {
+        g.addVertex('A');
+        expect(g.adjacencyList['A']).toMatchObject([]);
+    });
+
+    test('addEdge adds an edge between two nodes ', () => {
+        g.addVertex("A");
+        g.addVertex("B");
+        g.addEdge("A", "B");
+        expect(g.adjacencyList['A']).toMatchObject(['B']);
+        expect(g.adjacencyList['B']).toMatchObject(['A']);
+    });
+
+    test('removeEdge removes an edge between two nodes ', () => {
+        g.addVertex("A");
+        g.addVertex("B");
+        g.addVertex("C")
+        g.addEdge("A", "B");
+        g.addEdge("A", "C");
+        g.removeEdge("A", "B")
+        expect(g.adjacencyList['A']).toMatchObject(['C']);
+        expect(g.adjacencyList['B']).toMatchObject([]);
+    });
+
+    test('removeVertex removes all edges of that vertex and the vertex itself', () => {
+        g.addVertex("A");
+        g.addVertex("B");
+        g.addVertex("C")
+        g.addVertex("D")
+        g.addEdge("A", "B");
+        g.addEdge("A", "C");
+        g.addEdge("B", "C");
+        g.addEdge("B", "D");
+        g.addEdge("C", "D");
+        g.removeVertex('A');
+        expect(g.adjacencyList.hasOwnProperty('A')).toBe(false);
+        expect(g.adjacencyList['B']).toMatchObject(['C', 'D']);
+        expect(g.adjacencyList['C']).toMatchObject(['B', 'D']);
+    });
+});
+
+// g.addVertex("A")
+// g.addVertex("B")
+// g.addVertex("C")
+// g.addVertex("D")
+// g.addVertex("E")
+// g.addVertex("F")
+// g.addEdge("A", "B")
+// g.addEdge("A", "C")
+// g.addEdge("B","D")
+// g.addEdge("C","E")
+// g.addEdge("D","E")
+// g.addEdge("D","F")
+// g.addEdge("E","F")
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
